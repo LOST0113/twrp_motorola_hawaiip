13f5f5667141e00af41fb3751f50d93dea4730a5
LOST0113 <danicarys13@hotmail.com>
2024-02-24 12:08:57 -0300

    coment fastbootd and additional Libraries

--- device.mk
+++ device.mk
@@ -53,20 +53,20 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+# Fastbootd
+# PRODUCT_PACKAGES += \
+#     android.hardware.fastboot@1.0-impl-mock \
+#     fastbootd
 
-#Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
+# Additional Libraries
+# TARGET_RECOVERY_DEVICE_MODULES += \
+#     libkeymaster4 \
+#     libkeymaster41 \
+#     libpuresoftkeymasterdevice
 
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+# RECOVERY_LIBRARY_SOURCE_FILES += \
+#     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+#     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+#     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 
