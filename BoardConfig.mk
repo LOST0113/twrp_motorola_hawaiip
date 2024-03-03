4111f02e212d03bc2b995a5d47deb6e2116e7024
LOST0113 <danicarys13@hotmail.com>
2024-03-02 22:01:43 -0300

    add some things to TWRP Config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,24 +138,22 @@ TARGET_HW_DISK_ENCRYPTION := false
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
 TW_DEFAULT_LANGUAGE := en_US
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_EXCLUDE_APEX := true
 TW_EXCLUDE_TWRPAPP := true
 TARGET_USES_MKE2FS := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_SCREEN_BLANK_ON_BOOT := true
 
-# TW_SCREEN_BLANK_ON_BOOT := true
 # TW_NO_SCREEN_BLANK := true
 
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 TW_DEFAULT_BRIGHTNESS := 1200
-
-
-# TW_EXTRA_LANGUAGES := true
-
-# TW_INCLUDE_REPACKTOOLS := true
 
 # This device does not support fastboot boot, do *NOT* remove!
 TW_NO_FASTBOOT_BOOT := true
